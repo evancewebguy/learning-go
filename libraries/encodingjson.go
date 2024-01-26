b9a2cfff49db6d4931557b0934c50765e9a6cca7
Evance Akello <evancewebguy@gmail.com>
2024-01-26 12:26:16 +0300

    just a little fun to pass time

--- libraries/encodingjson.go
+++ libraries/encodingjson.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"encoding/json"
+	"time"
+)
+
+/**
+	NOTE: rest apis have json as the standard way to communicate between services and Go standard library.
+          includes support for converting Go data types to and from JSON.
+	marshaling - means converting from a Go data type to an encoding.
+	unmarshalling - means converting tio a Go data type.
+*/
+
+//{
+//	"id":"12345",
+//	"date_ordered":"2020-05-01T13:01:02Z",
+//	"customer_id":"3",
+//	"items":[{"id":"xyz123","name":"Thing 1"},{"id":"abc789","name":"Thing 2"}]
+//}
+
+type Order struct {
+	ID          string    `json:"id"`
+	DateOrdered time.Time `json:"date_ordered"`
+	CustomerID  string    `json:"customer_id"`
+	Items       []Item    `json:"items"`
+}
+
+type Item struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (o Order) orderHandler() {
+	err := json.Unmarshal([]byte(data), &o)
+	if err != nil {
+		return
+	}
+}
+
+func main() {
+
+}
